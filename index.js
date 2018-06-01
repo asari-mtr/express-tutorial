deed73bd0ed93e5ffb0bb1f8de4be113ef62b082
Asari Mitsuteru <asari.mtr@gmail.com>
2018-06-01 23:32:53 +0900

    add post/all

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ app.get("/", function(req, res) {
     res.send("Hello World!");
 });
 
+app.post("/hello", function(req, res) {
+    res.send("You just called the post method at '/hello'!\n");
+});
+
+app.all("/test", function(req, res) {
+    res.send("HTTP method doesn't have any effect on this route!\n");
+});
+
 app.listen(3000);
