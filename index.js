554d82d77b703fb150c7fd2b773b20515a21deb1
Asari Mitsuteru <asari.mtr@gmail.com>
2018-06-01 23:36:46 +0900

    separate router

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.get("/", function(req, res) {
-    res.send("Hello World!");
-});
+var things = require('./things.js');
 
-app.post("/hello", function(req, res) {
-    res.send("You just called the post method at '/hello'!\n");
-});
-
-app.all("/test", function(req, res) {
-    res.send("HTTP method doesn't have any effect on this route!\n");
-});
+app.use('/things', things);
 
 app.listen(3000);
