ebe1019e3d9e7ffb40de9423c892df5f275aac13
Asari Mitsuteru <asari.mtr@gmail.com>
2018-06-03 12:52:31 +0900

    dataase

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
 https://www.tutorialspoint.com/expressjs/index.htm
 
 ``` sh
-npm install -g nodemon
+$ npm install -g nodemon
 
-nodemon index.js
+$ nodemon index.js
+```
+
+use db
+
+``` sh
+$ brew install mongodb
+$ brew searvices start mongodb
+$ mongo
+> use my_db
+```
+
+update person
+```
+$ curl -X PUT --data "name=James&age=20&nationality=American" http://localhost:3000/people/5b135fcde739c49632546395
+$ curl -X DELETE http://localhost:3000/people/5b135fcde739c49632546395
 ```
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var multer = require('multer');
 var upload = multer();
 var app = express();
+var mongoose = require('mongoose');
 
 app.get('/', function(req, res) {
     res.render('form');
@@ -25,4 +26,63 @@ app.post('/', function(req, res) {
     console.log(req.body);
     res.send("received your request!");
 });
+
+mongoose.connect('mongodb://localhost/my_db');
+
+var personSchema = mongoose.Schema({
+    name: String,
+    age: Number,
+    nationality: String
+});
+
+var Person = mongoose.model("Person", personSchema);
+
+app.get('/person', function(req, res) {
+    res.render('person');
+});
+
+app.post('/person', function(req, res) {
+    var personInfo = req.body;
+
+    if(!personInfo.name || !personInfo.age || !personInfo.nationality) {
+        res.render('show_message', {
+            message: "Sorry, you provided wrong info", type: "error"
+        });
+    } else {
+        var newPerson = new Person({
+            name: personInfo.name,
+            age: personInfo.age,
+            nationality: personInfo.nationality
+        });
+
+        newPerson.save(function(err, Person) {
+            if(err) {
+                res.render('show_message', {message: "Database error", type: "error"});
+            } else {
+                res.render('show_message', {message: "New person added", type: "success", person: personInfo});
+            }
+        });
+    }
+});
+
+app.get('/people', function(req, res) {
+    Person.find(function(err, response) {
+        res.json(response);
+    });
+});
+
+app.put('/people/:id', function(req, res) {
+    Person.findByIdAndUpdate(req.params.id, req.body, function(err, response) {
+        if(err) res.json({message: "Error in updating person with id " + req.params.id});
+        res.json(response);
+    });
+});
+
+app.delete('/people/:id', function(req, res) {
+    Person.findByIdAndRemove(req.params.id, function(err, response) {
+        if(err) res.json({message: "Error in deleting person with id " + req.params.id});
+        res.json({message: "Person with id " + req.params.id + " removed."});
+    });
+});
+
 app.listen(3000);
