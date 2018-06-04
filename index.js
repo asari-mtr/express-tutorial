8d0a0f93ca7ec2bbc6e16857467823b031c99667
Asari Mitsuteru <asari.mtr@gmail.com>
2018-06-05 00:09:07 +0900

    authenticate(does not work login)

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
 var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
 var multer = require('multer');
 var upload = multer();
-var app = express();
 var mongoose = require('mongoose');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.use(cookieParser());
-app.use(session({secret: "Shh, its a secret!"}));
-
-// for parsing application/json
 app.use(bodyParser.json());
-
-// for parsing application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// for parsing multipart/form-data
 app.use(upload.array());
+app.use(cookieParser());
 app.use(express.static('public'));
+app.use(session({secret: "Shh, its a secret!"}));
 
 app.get('/', function(req, res) {
     if(req.session.page_views) {
@@ -99,6 +93,77 @@ app.delete('/people/:id', function(req, res) {
         if(err) res.json({message: "Error in deleting person with id " + req.params.id});
         res.json({message: "Person with id " + req.params.id + " removed."});
     });
+});
+
+var Users = [];
+
+app.get('/signup', function(req, res) {
+    res.render('signup');
+});
+
+app.post('/signup', function(req, res) {
+    if(!req.body.id || !req.body.password) {
+        res.status("400");
+        res.send("Invalid details!");
+    } else {
+        Users.filter(function(user) {
+            if(user.id === req.body.id) {
+                res.render('signup', {
+                    message: "User Already Exists! Login or choose another user id"});
+            }
+        });
+        var newUser = {id: req.body.id, password: req.body.password};
+        Users.push(newUser);
+        req.session.user = newUser;
+        res.redirect('/protected_page');
+    }
+});
+
+function checkSignIn(req, res, next) {
+    if(req.session.user){
+        return next();
+    } else {
+        var err = new Error("Not logged in!");
+        console.log(req.session.user);
+        return next(err);
+    }
+}
+
+app.get('/protected_page', checkSignIn, function(req, res) {
+    res.render('protected_page', {id: req.session.user.id});
+});
+
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+
+app.post('/login', function(req, res) {
+    console.log(Users);
+    console.log(req.body);
+    if(!req.body.id || !req.body.password){
+        res.render('login', {message: "Please enter both id and password"});
+    } else {
+        Users.filter(function(user) {
+            if(user.id === req.body.id && user.password === req.body.password) {
+                console.log("success");
+                console.log(user);
+                req.session.user = user;
+                res.redirect('/protected_page');
+            }
+        });
+        res.render('login', {message: "Invalid credentials!"});
+    }
+});
+
+app.get("/logout", function(req, res) {
+    req.session.destroy(function() {
+        console.log("user logged out.");
+    });
+    res.redirect('/login');
+});
+
+app.get('/protected_page', function(req, res) {
+    res.redirect('/login');
 });
 
 app.listen(3000);
